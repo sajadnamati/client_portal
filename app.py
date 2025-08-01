62d8ac32bdf462fbedb1d0441fd9f4271922f0b7
SajjadN <sadjad.neamati@gmail.com>
2025-08-01 20:07:58 +0330

    Initial commit for client portal app

---
+++ app.py
@@ -0,0 +1,40 @@
+import os
+from flask import Flask, redirect, url_for, session, render_template
+from authlib.integrations.flask_client import OAuth
+
+app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY")
+
+oauth = OAuth(app)
+google = oauth.register(
+    name='google',
+    client_id=os.environ.get("GOOGLE_CLIENT_ID"),
+    client_secret=os.environ.get("GOOGLE_CLIENT_SECRET"),
+    server_metadata_url='https://accounts.google.com/.well-known/openid-configuration',
+    client_kwargs={
+        'scope': 'openid email profile'
+    }
+)
+
+@app.route('/')
+def homepage():
+    user = dict(session).get('user', None)
+    return render_template('index.html', user=user)
+
+@app.route('/login')
+def login():
+    redirect_uri = url_for('authorize', _external=True)
+    return google.authorize_redirect(redirect_uri)
+
+@app.route('/authorize')
+def authorize():
+    token = google.authorize_access_token()
+    resp = google.get('userinfo')
+    user_info = resp.json()
+    session['user'] = user_info
+    return redirect('/')
+
+@app.route('/logout')
+def logout():
+    session.pop('user', None)
+    return redirect('/')
